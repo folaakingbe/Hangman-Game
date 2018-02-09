3ffd7541f64ace88587a3da955b184a9b5564d9d
Fola <folaakingbe@yahoo.com>
2018-02-08 23:53:20 -0500

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,58 @@
 // Global variables
+
+// Letters Guessed by user
 var guessedLetters = [];
+// Win Count
 var winCount = 0;
+// Lose count
 var lossCount = 0;
+// Message for Winner or Loser
 var winOrLose = "";
-var possibleWords = ["Blaziken", "Absol", "Delphox", "Flygon", "Volcarona", "Mienshao", "Zangoose", "Weavile", "Pyroar", "Luxray"];
+// Possible hidden words (Smaller List is commented)
+var possibleWords = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill",
+                    "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran", "Nidorina",
+                    "Nidoqueen", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras",
+                    "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl",
+                    "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem",
+                    "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch'd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster",
+                    "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee",
+                    "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie",
+                    "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Tauros", "Eevee", "Vaporeon", "Jolteon", "Flareon",
+                    "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew"];
+// var possibleWords = ["Blaziken", "Absol", "Delphox", "Flygon", "Volcarona", "Mienshao", "Zangoose", "Weavile", "Pyroar", "Luxray"];
+
+// Boolean if new word is needed
 var newWord = true;
+// Solved word
 var mysteryWord = "";
+// Word as it is being solved
 var hiddenWord = "";
+// Word as it is being solved in array form
 var hangman = [];
+// Number of Guesses
 var guessesLeft = 12;
+// Letters of the alphabet
 var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+// User input
 var userGuess = "";
+// Boolean if actual letter is pressed
 var realLetter = false;
 
 // Functions
+
+// Returns a random word from the list
 function getNewWord() {
     newWord = false;
     word = possibleWords[Math.floor(Math.random() * possibleWords.length)];
     return word.toUpperCase();
 }
 
-// Replace each letter in array except for periods and spaces with underlines
+// Replace each letter in array except for periods and spaces and such with underlines
 function hideWord(secretWord) {
     // var lengthOfWord = secretWord.length;
     // var numberOfGuesses = blank.length;
     for (i = 0; i < secretWord.length; i++) {
-        if (secretWord.charAt(i) === " " || secretWord.charAt(i) === "." || secretWord.charAt(i) === "-") {
+        if (secretWord.charAt(i) === " " || secretWord.charAt(i) === "." || secretWord.charAt(i) === "-" || secretWord.charAt(i) === "'") {
             hangman[i] = secretWord.charAt(i);
         }
         else {
@@ -35,6 +62,7 @@ function hideWord(secretWord) {
     updateHiddenWord();
 }
 
+// Fills word after each input
 function fillWord(blank) {
     var newLetter = true;
     var foundLetter = false;
@@ -57,6 +85,7 @@ function fillWord(blank) {
     }
 }
 
+// 
 function updateHiddenWord() {
     var update = "";
     for (i = 0; i < hangman.length; i++) {
@@ -98,14 +127,15 @@ function winLose() {
         document.getElementById("result").innerHTML = winOrLose;
         winCount = winCount + 1;
         newWord = true;
-        // resetWord();
+        
     }
     else if (guessesLeft === 0) {
         winOrLose = "You Lose...";
         document.getElementById("result").innerHTML = winOrLose;
+        document.getElementById("hidden").innerHTML = mysteryWord;
         lossCount = lossCount + 1;
         newWord = true;
-        // resetWord();
+        
     }
 }
 
@@ -117,19 +147,9 @@ document.onkeyup = function (event) {
     document.getElementById("wins").innerHTML = winCount;
     document.getElementById("losses").innerHTML = lossCount;
     // Get a new hidden word if needed
-
-    // if (newWord === true) {
-    //     // mysteryWord = getNewWord();
-    //     // hideWord(mysteryWord);
-    //     resetWord();
-    // }
-    // User guesses letter
     userGuess = String.fromCharCode(event.which).toUpperCase();
     if (newWord === true) {
         resetWord();
-        // document.getElementById("hidden").innerHTML = hangman.join(" ");
-        // document.getElementById("guess").innerHTML = guessesLeft;
-        // document.getElementById("letters").innerHTML = guessedLetters;
     }
 
     else {
@@ -142,19 +162,6 @@ document.onkeyup = function (event) {
         document.getElementById("hidden").innerHTML = hangman.join(" ");
         document.getElementById("guess").innerHTML = guessesLeft;
         document.getElementById("letters").innerHTML = guessedLetters;
-        if (hiddenWord === mysteryWord) {
-            winOrLose = "You Win!";
-            document.getElementById("result").innerHTML = winOrLose;
-            winCount = winCount + 1;
-            newWord = true;
-            // resetWord();
-        }
-        else if (guessesLeft === 0) {
-            winOrLose = "You Lose...";
-            document.getElementById("result").innerHTML = winOrLose;
-            lossCount = lossCount + 1;
-            newWord = true;
-            // resetWord();
-        }
+        winLose();
     }
 }
